6a5a902913bceb09548dd0eecaa51f945570a77c
Melissa Tarbox <mtarbox13@gmail.com>
2016-07-25 22:26:52 -0400

    styling custome post

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,8 +12,9 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-        <h1>This is my custome post page</h1>
+				<article-class="case-study">
 				<?php the_content(); ?>
+			</article>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
