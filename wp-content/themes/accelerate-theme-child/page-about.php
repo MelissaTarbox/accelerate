0217d4b080568f98822c3db31c86c61f91385135
Melissa Tarbox <mtarbox13@gmail.com>
2016-08-04 19:42:32 -0400

    got image to show

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -9,13 +9,27 @@
  */
 
 get_header(); ?>
-<section class="page-about">
-	<div class="site-content">
+<section class="page-about-header">
+	<div class="about-header">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+				<div class="about-hero">
+					<p><span class="green">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</p>
+				</div>
 			<?php endwhile; // end of the loop. ?>
 
-		</div><!-- site-content -->
-	</section><!--about-page-->
+		</div><!-- about header -->
+	</section><!--about-page-header-->
+
+	<section class="about-list">
+		<div class-"about-content">
+			<ul class="about-featured-services">
+				<?php while ( have_posts() ) : the_post(); ?>
+						<?php the_content();?>
+					<?php endwhile; //end of the loop ?>
+					<?php wp_resent_query(); //resets altered query to original ?>
+				</ul>
+			</div>
+		</section>
+
 
 <?php get_footer(); ?>
